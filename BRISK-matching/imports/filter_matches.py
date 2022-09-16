941f92fd2f13217af0ed0500aec89928457dcf2a
Aakash Shrestha <aakash.shres@gmail.com>
2022-09-16 12:29:59 -0600

    Brute force matcing

--- BRISK-matching/imports/filter_matches.py
+++ BRISK-matching/imports/filter_matches.py
@@ -1,12 +1,15 @@
 import numpy as np
-def filter_matches(kp1, kp2, matches, ratio = 0.75):
+
+
+def filter_matches(kp1, kp2, matches, ratio=0.75):
     # Lows ratio test
-    mkp1, mkp2, good= [], [], []
+    mkp1, mkp2, good = [], [], []
+    len(matches)
     for m in matches:
         if len(m) == 2 and m[0].distance < m[1].distance * ratio:
             m = m[0]
-            mkp1.append( kp1[m.queryIdx] )
-            mkp2.append( kp2[m.trainIdx] )
+            mkp1.append(kp1[m.queryIdx])
+            mkp2.append(kp2[m.trainIdx])
             good.append(m)
     p1 = np.float32([kp.pt for kp in mkp1])
     p2 = np.float32([kp.pt for kp in mkp2])
